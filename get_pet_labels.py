bc8aad8ef2512b0552f71f42192400bd2d5c51b7
malisaroj <malisaroj@icloud.com>
2022-08-21 06:56:29 +0545

    Update get_pet_labels.py

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -17,7 +17,7 @@
 #
 ##
 # Imports python modules
-from os import listdir, scandir
+from os import listdir
 
 # TODO 2: Define get_pet_labels function below please be certain to replace None
 #       in the return statement with results_dic dictionary that you create 
@@ -40,79 +40,48 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
-    # Retrieve the filenames from folder pet_images/
+    # Creates list of files in directory
+    in_files = listdir(image_dir)
     
-    filename_list = listdir(image_dir)
-    count = 0
-    pet_image_list = []
-    filenames = []
-    pet_labels = []
-
-# Print 10 of the filenames from folder pet_images/
-    for path in scandir(image_dir):
-        if path.is_file():
-            count += 1
-        
-    print("\nPrints filenames from folder pet_images/")
-    for idx in range(count):
-        print("{:2d} file: {:>25}".format(idx + 1, filename_list[idx]) )
-
-
-        pet_image_list.append(filename_list[idx])
-    # Sets pet_image variable to a filename 
-    for pets in pet_image_list:
-        pet_image = pets
-    #pet_image = "Boston_terrier_02259.jpg"
-
-        # Sets string to lower case letters
-        low_pet_image = pet_image.lower()
-
-        # Splits lower case string by _ to break into words 
-        word_list_pet_image = low_pet_image.split("_")
-
-        # Create pet_name starting as empty string
-        pet_name = ""
-
-        # Loops to check if word in pet name is only
-        # alphabetic characters - if true append word
-        # to pet_name separated by trailing space 
-        for word in word_list_pet_image:
-            if word.isalpha():
-                pet_name += word + " "
-
-        # Strip off starting/trailing whitespace characters 
-        pet_name = pet_name.strip()
-
-        # Prints resulting pet_name
-        #print("\nFilename=", pet_image, "   Label=", pet_name)
-        filenames.append(pet_image)
-        pet_labels.append(pet_name)
-
-    # Creates empty dictionary named results_dic
+    # Processes each of the files to create a dictionary where the key
+    # is the filename and the value is the picture label (below).
+ 
+    # Creates empty dictionary for the results (pet labels, etc.)
     results_dic = dict()
-
-    # Determines number of items in dictionary
-    items_in_dic = len(results_dic)
-    print("\nEmpty Dictionary results_dic - n items=", items_in_dic)
-
-    # Adds new key-value pairs to dictionary ONLY when key doesn't already exist. This dictionary's value is
-    # a List that contains only one item - the pet image label
-    #filenames = ["beagle_0239.jpg", "Boston_terrier_02259.jpg"]
-    #pet_labels = ["beagle", "boston terrier"]
-    for idx in range(0, len(filenames), 1):
-        if filenames[idx] not in results_dic:
-             results_dic[filenames[idx]] = [pet_labels[idx]]
-        else:
-             print("** Warning: Key=", filenames[idx], 
-                   "already exists in results_dic with value =", 
-                   results_dic[filenames[idx]])
-
-    #Iterating through a dictionary printing all keys & their associated values
-    print("\nPrinting all key-value pairs in dictionary results_dic:")
-    for key in results_dic:
-        print("Filename=", key, "   Pet Label=", results_dic[key][0])
-        
+   
+    # Processes through each file in the directory, extracting only the words
+    # of the file that contain the pet image label
+    for idx in range(0, len(in_files), 1):
        
-    # Replace None with the results_dic dictionary that you created with this
-    # function
-    return results_dic
+       # Skips file if starts with . (like .DS_Store of Mac OSX) because it 
+       # isn't an pet image file
+       if in_files[idx][0] != ".":
+           
+           # Sets string to lower case letters
+           # Splits lower case string by _ to break into words 
+           pet_image = in_files[idx].lower().split("_")
+            
+           # Creates temporary label variable to hold pet label name extracted 
+           pet_label = ""
+
+           # Loops to check if word in pet name is only
+           # alphabetic characters - if true append word
+           # to pet_name separated by trailing space 
+           for word in pet_image:
+               if word.isalpha():
+                   pet_label += word + " "
+
+           # Strip off starting/trailing whitespace characters 
+           pet_label = pet_label.strip()
+                
+           # If filename doesn't already exist in dictionary add it and it's
+           # pet label - otherwise print an error message because indicates 
+           # duplicate files (filenames)
+           if in_files[idx] not in results_dic:
+              results_dic[in_files[idx]] = [pet_label]
+              
+           else:
+               print("** Warning: Duplicate files exist in directory:", 
+                     in_files[idx])
+ 
+    return 
